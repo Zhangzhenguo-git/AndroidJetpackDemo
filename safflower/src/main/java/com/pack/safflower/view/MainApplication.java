c7b6477a9c4d14828d7a74be3f3d356c3b0ed10a
张振国 <zhangzhenguo123>
2020-11-12 17:42:27 +0800

    修改首页actionbar内容

---
+++ safflower/src/main/java/com/pack/safflower/view/MainApplication.java
@@ -0,0 +1,12 @@
+package com.pack.safflower.view;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+    
+}
